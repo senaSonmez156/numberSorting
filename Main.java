f83c446766b32e676f7ce36eafa3f8fa525b958f
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-04-13 19:02:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+package sýralama;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		Scanner input=new Scanner(System.in);
+		System.out.print("1.sayý: ");
+		a=input.nextInt();
+		System.out.print("2.sayý: ");
+		b=input.nextInt();
+		System.out.print("3.sayý: ");
+		c=input.nextInt();
+		
+		
+		if((a<b)&& (a<c)) {
+			if(b<c) {
+				System.out.print("a<b<c");
+			}
+			else {
+				System.out.print("a<c<b");
+			}
+		}
+		else if((b<a)&& (b<c)) {
+			if(a<c) {
+				System.out.print("b<a<c");
+			}
+			else {
+				System.out.print("b<c<a");
+			}
+		}
+		
+		
+		else {
+			if(b<a) {
+			System.out.print("c<b<a");
+		}
+		else {
+			System.out.print("c<a<b");
+		}}
+		
+		
+		
+		
+		
+
+	}
+
+}
